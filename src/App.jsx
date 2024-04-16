da2d46206fc25671bfdc5e6823e585361eddf215
Kholov Rustam <rustamkholow5@gmail.com>
2024-04-17 01:21:51 +0500

    Перенесите статические файлы (в нашем случае это будут только картинки) в папку public.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 
+
 function App() {
 
   return (
@@ -277,7 +278,7 @@ function App() {
 			<div className="container">
 				<div className="header__block">
 					<div className="header__logo _show _light">
-						<a href="" target="_self"><img src="images/logo.png" alt="logo"/></a>
+						<a href="" target="_self"><img src="/img/logo.png" alt="logo"/></a>
 					</div>
 					<div className="header__logo _dark">
 						<a href="" target="_self"><img src="images/logo_dark.png" alt="logo"/></a>
