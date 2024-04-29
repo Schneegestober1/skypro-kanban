5cc00cd33506ccbb922d1e7b9cd16ad6d5503703
Kholov Rustam <rustamkholow5@gmail.com>
2024-04-29 22:53:45 +0500

    pop-exit__exit-yes

--- src/componets/Popups/Popuser/PopUser.jsx
+++ src/componets/Popups/Popuser/PopUser.jsx
@@ -1,19 +1,21 @@
+import { PopExit, PopExitBlock, PopExitContainer, PopExitFormGroup, PopExitTtl, PopExitTtlH2 } from "./popUser.styled"
+
 export const PopUSer= () => {
     return (
-        <div className="pop-exit" id="popExit"> 
-				<div className="pop-exit__container">
-					<div className="pop-exit__block">
-						<div className="pop-exit__ttl">
-							<h2>Выйти из аккаунта?</h2>
-						</div>
+        <PopExit id="popExit"> 
+				<PopExitContainer>
+					<PopExitBlock>
+						<PopExitTtl>
+							<PopExitTtlH2>Выйти из аккаунта?</PopExitTtlH2>
+						</PopExitTtl>
 						<form className="pop-exit__form" id="formExit" action="#">
-							<div className="pop-exit__form-group">
+							<PopExitFormGroup>
 								<button className="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
 								<button className="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
-							</div>
+							</PopExitFormGroup>
 						</form>
-					</div>
-				</div>
-        </div>
+					</PopExitBlock>
+				</PopExitContainer>
+        </PopExit>
     )
 }
