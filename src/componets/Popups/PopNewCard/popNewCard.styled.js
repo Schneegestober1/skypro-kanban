e3e6730bc08c892cffc3f67e7847b432b6a4ebe0
Kholov Rustam <rustamkholow5@gmail.com>
2024-05-27 22:22:46 +0500

    opacity: ${({$isActive}) => $isActive ? '1' : '0.4'};

--- src/componets/Popups/PopNewCard/popNewCard.styled.js
+++ src/componets/Popups/PopNewCard/popNewCard.styled.js
@@ -8,3 +8,11 @@ font-size: 12px;
 margin: 0;
 --rdp-accent-color: #c4c4c4;
 `
+
+export const RadioInput = styled.input`
+display: none;
+`
+
+export const WrapperRadio = styled.div`
+opacity: ${({$isActive}) => $isActive ? '1' : '0.4'};
+`
