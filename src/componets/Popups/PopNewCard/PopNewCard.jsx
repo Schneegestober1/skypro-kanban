cb280f50781f7cee8ba1fa789edfc29e9b478072
Kholov Rustam <rustamkholow5@gmail.com>
2024-05-29 21:28:09 +0500

    form-new__create

--- src/componets/Popups/PopNewCard/PopNewCard.jsx
+++ src/componets/Popups/PopNewCard/PopNewCard.jsx
@@ -1,11 +1,11 @@
 import { useContext, useState } from "react"
 import { addNewCard } from "../../../api/cardsApi"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { paths } from "../../../routesPaths"
 import { UserContext } from "../../../context/userContext"
 import { CardsContext } from "../../../context/cardsContext"
 import { ErrorPopNewCard } from "../../../pages/RegisterPage/registerPage.styled"
-import { DP, RadioInput, WrapperRadio } from "./popNewCard.styled"
+import { CalendarTtl, CategoriesP, CategoriesThemes, DP, FormNewArea, FormNewBlock, FormNewCreate, FormNewInput, PopNewCardBlock, PopNewCardCalendar, PopNewCardCategoriesCategories, PopNewCardClose, PopNewCardContainer, PopNewCardContent, PopNewCardDiv, PopNewCardForm, PopNewCardTtl, PopNewCardWrap, RadioInput, SubttlLabel, WrapperRadio } from "./popNewCard.styled"
 
 export const PopNewCard= () => {
     const {user} = useContext(UserContext)
@@ -62,51 +62,51 @@ export const PopNewCard= () => {
     console.log(topic);
     
     return (
-        <div className="pop-new-card" id="popNewCard">
-        <div className="pop-new-card__container">
-            <div className="pop-new-card__block">
-                <div className="pop-new-card__content">
-                    <h3 className="pop-new-card__ttl">Создание задачи</h3>
-                    <Link to={paths.MAIN} className="pop-new-card__close">&#10006;</Link>
-                    <div className="pop-new-card__wrap">
-                        <form className="pop-new-card__form form-new" id="formNewCard" action="#">
-                            <div className="form-new__block">
-                                <label htmlFor="formTitle" className="subttl">Название задачи</label>
-                                <input onChange={onChangeInput} className="form-new__input" type="text" name="title" id="formTitle" placeholder="Введите название задачи..." autoFocus/>
-                            </div>
-                            <div className="form-new__block">
-                                <label htmlFor="textArea" className="subttl">Описание задачи</label>
-                                <textarea onChange={onChangeInput} className="form-new__area" name="description" id="textArea"  placeholder="Введите описание задачи..."></textarea>
-                            </div>
-                        </form>
-                        <div className="pop-new-card__calendar calendar">
-                            <p className="calendar__ttl subttl">Даты</p>
-                            <DP mode="single" selected={date} onSelect={setDate} footer={getDateFormat(date)}/>
-                        </div>
-                    </div>
-                    <div className="pop-new-card__categories categories">
-                        <p className="categories__p subttl">Категория</p>
-                        <div className="categories__themes">
-                            <WrapperRadio $isActive={topic === 'Web Design'} className="categories__theme _orange">
-                                <label htmlFor="radio1">Web Design</label>
-                                <RadioInput onChange={(e) => setTopic(e.target.value)} id="radio1" type="radio" name="keks" value={'Web Design'}/> 
-                            </WrapperRadio>
-                            <WrapperRadio $isActive={topic === 'Research'} className="categories__theme _green">
-                                <label htmlFor="radio2">Research</label>
-                                <RadioInput onChange={(e) => setTopic(e.target.value)} className="_green" id="radio2" type="radio" name="keks" value={'Research'}/>
-                            </WrapperRadio>
-                            <WrapperRadio $isActive={topic === 'Copywriting'} className="categories__theme _purple">
-                                <label htmlFor="radio3">Copywriting</label>
-                                <RadioInput onChange={(e) => setTopic(e.target.value)} className="_purple" id="radio3" type="radio" name="keks" value={'Copywriting'}/>
-                            </WrapperRadio>
-                        </div>
-                    </div>
-                    <ErrorPopNewCard>{error && error}</ErrorPopNewCard>
-                    {/* перенести из RP */}
-                    <button onClick={onAddNewCard} className="form-new__create _hover01" id="btnCreate">Создать задачу</button>
-                </div>
-            </div>
-        </div>
-        </div>
+        <PopNewCardDiv id="popNewCard">
+            <PopNewCardContainer>
+                <PopNewCardBlock>
+                    <PopNewCardContent>
+                        <PopNewCardTtl>Создание задачи</PopNewCardTtl>
+                        <PopNewCardClose to={paths.MAIN}>&#10006;</PopNewCardClose>
+                        <PopNewCardWrap>
+                            <PopNewCardForm className="form-new" id="formNewCard" action="#">
+                                <FormNewBlock>
+                                    <SubttlLabel htmlFor="formTitle">Название задачи</SubttlLabel>
+                                    <FormNewInput onChange={onChangeInput} className="form-new__input" type="text" name="title" id="formTitle" placeholder="Введите название задачи..." autoFocus/>
+                                </FormNewBlock>
+                                <FormNewBlock>
+                                    <SubttlLabel htmlFor="textArea">Описание задачи</SubttlLabel>
+                                    <FormNewArea onChange={onChangeInput} name="description" id="textArea"  placeholder="Введите описание задачи..."></FormNewArea>
+                                </FormNewBlock>
+                            </PopNewCardForm>
+                            <PopNewCardCalendar>
+                                <CalendarTtl>Даты</CalendarTtl>
+                                <DP mode="single" selected={date} onSelect={setDate} footer={getDateFormat(date)}/>
+                            </PopNewCardCalendar>
+                        </PopNewCardWrap>
+                        <PopNewCardCategoriesCategories>
+                            <CategoriesP>Категория</CategoriesP>
+                            <CategoriesThemes>
+                                <WrapperRadio $isActive={topic === 'Web Design'} className="categories__theme _orange">
+                                    <label htmlFor="radio1">Web Design</label>
+                                    <RadioInput onChange={(e) => setTopic(e.target.value)} id="radio1" type="radio" name="keks" value={'Web Design'}/> 
+                                </WrapperRadio>
+                                <WrapperRadio $isActive={topic === 'Research'} className="categories__theme _green">
+                                    <label htmlFor="radio2">Research</label>
+                                    <RadioInput onChange={(e) => setTopic(e.target.value)} className="_green" id="radio2" type="radio" name="keks" value={'Research'}/>
+                                </WrapperRadio>
+                                <WrapperRadio $isActive={topic === 'Copywriting'} className="categories__theme _purple">
+                                    <label htmlFor="radio3">Copywriting</label>
+                                    <RadioInput onChange={(e) => setTopic(e.target.value)} className="_purple" id="radio3" type="radio" name="keks" value={'Copywriting'}/>
+                                </WrapperRadio>
+                            </CategoriesThemes>
+                        </PopNewCardCategoriesCategories>
+                        <ErrorPopNewCard>{error && error}</ErrorPopNewCard>
+                        {/* перенести из RP */}
+                        <FormNewCreate onClick={onAddNewCard} id="btnCreate">Создать задачу</FormNewCreate>
+                    </PopNewCardContent>
+                </PopNewCardBlock>
+            </PopNewCardContainer>
+        </PopNewCardDiv>
     )
 }
--- src/componets/Popups/PopNewCard/popNewCard.styled.js
+++ src/componets/Popups/PopNewCard/popNewCard.styled.js
@@ -1,5 +1,230 @@
 import { DayPicker } from "react-day-picker";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Hover01, colorTheme } from "../../../global.styled";
+
+export const PopNewCardDiv = styled.div`
+  width: 100%;
+  min-width: 375px;
+  height: 100%;
+  min-height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 6;
+
+@media screen and (max-width: 660px) {
+  top: 70px;
+}
+`
+
+export const PopNewCardContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  padding: 0 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.4);
+
+  @media screen and (max-width: 660px) {
+    padding: 0;
+    justify-content: flex-start;
+  }
+
+  @media screen and (max-width: 495px) {
+    padding: 0;
+    justify-content: flex-start;
+  }
+`
+
+export const PopNewCardBlock = styled.div`
+  display: block;
+  margin: 0 auto;
+  background-color: #FFFFFF;
+  max-width: 630px;
+  width: 100%;
+  padding: 40px 30px 48px;
+  border-radius: 10px;
+  border: 0.7px solid #D4DBE5;
+  position: relative;
+
+  @media screen and (max-width: 660px) {
+    border-radius: 0;
+  }
+
+  @media screen and (max-width: 495px) {
+    padding: 20px 16px 32px;
+  }
+`
+
+export const PopNewCardContent = styled.div`
+  display: block;
+  text-align: left;
+`
+
+export const PopNewCardTtl = styled.h3`
+  color: #000;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 24px;
+  margin-bottom: 20px;
+`
+
+export const PopNewCardClose = styled(Link)`
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  color: #94A6BE;
+  cursor: pointer;
+
+  &:hover {
+    color: #000000;
+  }
+`
+
+export const PopNewCardWrap = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`
+
+export const PopNewCardForm = styled.form`
+  max-width: 370px;
+  width: 100%;
+  display: block;
+  margin-bottom: 20px;
+`
+
+export const FormNewBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const SubttlLabel = styled.label`
+  color: #000;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+`
+
+export const FormNewInput = styled.input`
+  width: 100%;
+  outline: none;
+  padding: 14px;
+  background: transparent;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  border-radius: 8px;
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: -0.14px;
+  margin: 20px 0;
+
+  &::-moz-placeholder {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1px;
+  color: #94A6BE;
+  letter-spacing: -0.14px;
+  }
+
+  &::placeholder {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1px;
+  color: #94A6BE;
+  letter-spacing: -0.14px;
+  }
+`
+
+export const FormNewArea = styled.textarea`
+  width: 100%;
+  outline: none;
+  padding: 14px;
+  background: transparent;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  border-radius: 8px;
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: -0.14px;
+  max-width: 370px;
+  margin-top: 14px;
+  height: 200px;
+
+  &::-moz-placeholder {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1px;
+  color: #94A6BE;
+  letter-spacing: -0.14px;
+  }
+
+  &::placeholder {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1px;
+  color: #94A6BE;
+  letter-spacing: -0.14px;
+ }
+`
+
+export const PopNewCardCalendar = styled.div`
+width: 182px;
+margin-bottom: 20px;
+@media screen and (max-width: 660px) {
+    max-width: 340px;
+    width: 100%;
+    display: none;
+    margin-bottom: 7px;
+}
+`
+
+export const CalendarTtl = styled.p`
+  margin-bottom: 14px;
+  padding: 0 7px;
+  color: #000;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+`
+
+export const PopNewCardCategoriesCategories = styled.div`
+  margin-bottom: 20px;
+`
+
+export const CategoriesP = styled.p`
+  margin-bottom: 14px;
+  color: #000;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+`
+
+export const CategoriesThemes = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: flex-start;
+  justify-content: flex-start;
+`
+
+export const FormNewCreate = styled.button`
+  width: 132px;
+  height: 30px;
+  background-color: #565EEF;
+  border-radius: 4px;
+  border: 0;
+  outline: none;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1;
+  color: #FFFFFF;
+  float: right;
+
+  ${Hover01}
+`
+
 
 export const DP = styled(DayPicker)`
 --rdp-cell-size: 22px;
@@ -15,4 +240,6 @@ display: none;
 
 export const WrapperRadio = styled.div`
 opacity: ${({$isActive}) => $isActive ? '1' : '0.4'};
+${(props) => colorTheme(props.$color)}
+
 `
